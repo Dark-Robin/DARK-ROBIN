c6be189f8e553a7db4dbd9437ac889295d7ae60f
Dark-Robin <sihileld23@gmail.com>
2024-12-28 23:53:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,5 +26,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "KJFm1YTR#jZD7sQYQ8SzZZdJKOjlbyNBew1Xf_7o4t8sMyY1NeDc",
 MONGODB: process.env.MONGODB || "mongodb://mongo:wUgAuMxTmGvnyihBBnKiTvNIOiUiYSSU@autorack.proxy.rlwy.net:54588",
 OWNER_NUM: process.env.OWNER_NUM || "94704101989", // Remove this number and add your number with country code
-OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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",
 };
