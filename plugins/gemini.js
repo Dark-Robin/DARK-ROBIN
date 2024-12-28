55bfa102aee830c7bab71bdad1e7d7426e87cddf
Dark-Robin <sihileld23@gmail.com>
2024-12-28 23:44:10 +0530

    Update gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -3,7 +3,7 @@ const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY, // Use environment variable
+    apiKey: config.OPENAI_API_KEY, // Use environment variable
 });
 const openai = new OpenAIApi(configuration);
 
